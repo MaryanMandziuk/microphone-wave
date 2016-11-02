fa2faa0cc9af9e756aca8a57bd39243bab33794c
Maryan Mandziuk <maryan.mandziuk@gmail.com>
2016-11-02 22:40:10 +0200

    Added captured data saving

--- src/microphonevisualizer/WaveDrawing.java
+++ src/microphonevisualizer/WaveDrawing.java
@@ -14,12 +14,20 @@ import java.awt.Graphics;
 import java.awt.Graphics2D;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.io.ByteArrayInputStream;
+import java.io.File;
+import java.io.IOException;
 
 import java.nio.ByteBuffer;
+import java.nio.ByteOrder;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import javax.sound.sampled.AudioFileFormat;
 
 import javax.sound.sampled.AudioFormat;
+import javax.sound.sampled.AudioInputStream;
 import javax.sound.sampled.AudioSystem;
 import javax.sound.sampled.DataLine;
 import javax.sound.sampled.LineUnavailableException;
@@ -126,10 +134,12 @@ class MyPanel extends JPanel implements ActionListener {
             if (!AudioSystem.isLineSupported(info)) {
                 System.out.println("Line doesn't supported. ");
             }
-            
+
             line = (TargetDataLine) AudioSystem.getLine(info);
             line.open(format);
             line.start();
+            
+
         } catch (LineUnavailableException ex) {
             System.out.println("Line unavailable " + ex);
         }
@@ -145,9 +155,10 @@ class MyPanel extends JPanel implements ActionListener {
         ByteBuffer bb = ByteBuffer.wrap(data);
         d = bb.getShort();
     }
-    
+
     /**
      * Reading sample
+     *
      * @return 16 bytes sample
      */
     private short getSample() {
@@ -157,9 +168,10 @@ class MyPanel extends JPanel implements ActionListener {
         ByteBuffer bb = ByteBuffer.wrap(data);
         return bb.getShort();
     }
-    
+
     /**
      * Capturing samples
+     *
      * @return short array of samples
      */
     private short[] getShortArray() {
@@ -168,15 +180,16 @@ class MyPanel extends JPanel implements ActionListener {
         for (int i = 0; i < len; i++) {
             data[i] = getSample();
         }
+        writeWAV(data);
         System.out.println("close");
         line.stop();
         line.close();
         return data;
     }
-    
+
     /**
-     * Scaled data
-     * method: average value in chunk
+     * Scaled data method: average value in chunk
+     *
      * @return short array
      */
     public short[] scaledData1() {
@@ -200,7 +213,7 @@ class MyPanel extends JPanel implements ActionListener {
             if (c1 == pointsScaler) {
                 scaledData[j] = (short) (sum / c1);
                 sum = 0;
-                c1 =0;
+                c1 = 0;
                 j++;
             }
             if (c2 == pointsScaler) {
@@ -212,10 +225,10 @@ class MyPanel extends JPanel implements ActionListener {
         }
         return scaledData;
     }
-    
+
     /**
-     * Scaled data
-     * method: highest value in chunk
+     * Scaled data method: highest value in chunk
+     *
      * @return short array
      */
     private short[] scaledData2() {
@@ -255,10 +268,10 @@ class MyPanel extends JPanel implements ActionListener {
         }
         return scaledData;
     }
-    
+
     /**
-     * Scaled data
-     * method: last value in chunk
+     * Scaled data method: last value in chunk
+     *
      * @return short array
      */
     private short[] scaledData3() {
@@ -274,7 +287,7 @@ class MyPanel extends JPanel implements ActionListener {
                 c1++;
             } else {
                 c2++;
-            } 
+            }
             if (c1 == pointsScaler) {
                 scaledData[j] = data[i];
                 c1 = 0;
@@ -289,9 +302,9 @@ class MyPanel extends JPanel implements ActionListener {
         return scaledData;
     }
 
-    
     /**
      * Getting envelope from audio data
+     *
      * @param arr
      * @return short array
      */
@@ -304,21 +317,22 @@ class MyPanel extends JPanel implements ActionListener {
     }
 
     /**
-    * Applying basic filter for envelope
-    */
+     * Applying basic filter for envelope
+     */
     public void filter(short[] envelope) {
         int N = 2;
         for (int i = 0; i < envelope.length; i++) {
             envelope[i] = getAverage(envelope, i, N);
         }
     }
-    
+
     /**
      * Getting average value from envelope N samples
+     *
      * @param data
      * @param index
      * @param N
-     * @return 
+     * @return
      */
     public short getAverage(short[] data, int index, int N) {
         int sum = 0;
@@ -381,46 +395,45 @@ class MyPanel extends JPanel implements ActionListener {
 
     @Override
     public void actionPerformed(ActionEvent ae) {
-        /*        readData();
-        Points p;
-        Color  c;
-        
-        if (count < 100) { 
-            count++;
-            sumOfSquares += Math.pow(Math.abs((int) (d / scaler)), 2);
-            rms = Math.sqrt(sumOfSquares / count);
-        } else {
+//        readData();
+//        Points p;
+//        Color c;
+//
+//        if (count < 100) {
+//            count++;
+//            sumOfSquares += Math.pow(Math.abs((int) (d / SCALER)), 2);
 //            rms = Math.sqrt(sumOfSquares / count);
-            count = 0;
-            sumOfSquares = 0;
-        }
-               
-        for(Points p1: points) {
-            p1.i1 = p1.i1 - 1;
-            p1.j1 = p1.j1 - 1;
-        }
-        dbCalculation();
-        if (rms < threshold ) {
-//        if (Math.abs(db)  > 36) {
-            c = Color.red;
-        } else {
-            c = Color.black;
-        }
-        
-        if ( points.isEmpty() ) {
-            p = new Points(800, 200, 800, 200 - (int) (d / scaler), c);
-            points.add(p); 
-        }  else {
-            int i2 = points.get(points.size() - 1).j2;
-            p = new Points(800, i2, 800, 200 - (int) (d / scaler), c);
-            points.add(p);
-
-        }
-        
-        if (points.size() > 801) {
-           points.remove(0);     
-        }
-         */
+//        } else {
+////            rms = Math.sqrt(sumOfSquares / count);
+//            count = 0;
+//            sumOfSquares = 0;
+//        }
+//
+//        for (Points p1 : points) {
+//            p1.i1 = p1.i1 - 1;
+//            p1.j1 = p1.j1 - 1;
+//        }
+//        dbCalculation();
+//        if (rms < 3.0) {
+////        if (Math.abs(db)  > 36) {
+//            c = Color.red;
+//        } else {
+//            c = Color.black;
+//        }
+//
+//        if (points.isEmpty()) {
+//            p = new Points(800, 200, 800, 200 - (int) (d / SCALER), c);
+//            points.add(p);
+//        } else {
+//            int i2 = points.get(points.size() - 1).j2;
+//            p = new Points(800, i2, 800, 200 - (int) (d / SCALER), c);
+//            points.add(p);
+//
+//        }
+//
+//        if (points.size() > 801) {
+//            points.remove(0);
+//        }
 
         repaint();
     }
@@ -437,11 +450,30 @@ class MyPanel extends JPanel implements ActionListener {
         }
         db = 20 * Math.log10((amplitude));
     }
+    
+    public void writeWAV(short[] data) {
+        ByteBuffer buffer = ByteBuffer.allocate(data.length * 2);
+        buffer.order(ByteOrder.BIG_ENDIAN);
+        buffer.asShortBuffer().put(data);
+        byte[] bytes = buffer.array();
+        
+        ByteArrayInputStream bais = new ByteArrayInputStream(bytes);
+        AudioInputStream audioInputStream = new AudioInputStream(bais,new AudioFormat(SAMPLERATE, 16, 1, true, true),bytes.length);
+        File fileOut = new File("test.wav");
+        if (AudioSystem.isFileTypeSupported(AudioFileFormat.Type.WAVE, 
+            audioInputStream)) {
+            try {
+                AudioSystem.write(audioInputStream, AudioFileFormat.Type.WAVE, fileOut);
+            } catch (IOException ex) {
+                System.out.println("Error: " + ex);
+            }
+        }
+    }
 }
 
 /**
  * class for representing wave's points
-*/
+ */
 class Points {
 
     public int i1;
